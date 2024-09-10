15595df9e9037738e1865fdfbd2dc47a0722114f
aravind452 <aravindaru452@gmail.com>
2024-09-10 20:44:26 +0530

    added reverse number

--- QSPIDERS/ReverseNumber.java
+++ QSPIDERS/ReverseNumber.java
@@ -1,37 +1,39 @@
 public class ReverseNumber {
 
   public static void main(String[] args) {
-    int original = 151;
-    int neworiginal = original;
-    int reverse = 0;
+    // int original = 151;
+    // int neworiginal = original;
+    // int reverse = 0;
 
-    while (neworiginal != 0) {
-      int temp = neworiginal % 10;
-      reverse = (reverse * 10) + temp;
-      neworiginal /= 10;
-    }
+    // while (neworiginal != 0) {
+    // int temp = neworiginal % 10;
+    // reverse = (reverse * 10) + temp;
+    // neworiginal /= 10;
+    // }
 
-    // System.out.println("Original Number " + original);
-    // System.out.println("Reversed Number " + reverse);
+    // // System.out.println("Original Number " + original);
+    // // System.out.println("Reversed Number " + reverse);
 
-    if (original == reverse) {
-      System.out.println("Palindrome");
-    } else {
-      System.out.println("Not palindrome");
-    }
+    // if (original == reverse) {
+    // System.out.println("Palindrome");
+    // } else {
+    // System.out.println("Not palindrome");
+    // }
 
     // Using String
 
-    // int original = 1234;
-    // String rev = String.valueOf(original);
-    // StringBuilder org = new StringBuilder(rev);
-    // StringBuilder sb = new StringBuilder(rev);
-    // sb = sb.reverse();
-    // if (sb.equals(org)) {
-    // System.out.println("Palindrome");
-    // } else {
-    // System.out.println("Not a palindrome");
-    // }
+    int original = 151;
+    String rev = String.valueOf(original);
+    StringBuilder org = new StringBuilder(rev);
+    StringBuilder sb = new StringBuilder(rev);
+    System.out.println(org);
+    sb = sb.reverse();
+    System.out.println(sb);
+    if (sb.toString().equals(org.toString())) {
+      System.out.println("Palindrome");
+    } else {
+      System.out.println("Not a palindrome");
+    }
 
   }
 }
