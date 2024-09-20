ae11e58b2e0e762ffb34d2c8bac82839533235c2
aravind452 <aravindaru452@gmail.com>
2024-09-20 21:32:09 +0530

    pattern 1-4 added

---
+++ QSPIDERS/Pattern2.java
@@ -0,0 +1,18 @@
+public class Pattern2 {
+
+  public static void main(String[] args) {
+    int n = 5;
+    char ch = 'A';
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i % 2 == 0) {
+          System.out.print(j + 1 + " ");
+        } else {
+          System.out.print(ch++ + " ");
+        }
+      }
+      ch = 'A';
+      System.out.println();
+    }
+  }
+}
