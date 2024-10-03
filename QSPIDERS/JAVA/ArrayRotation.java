c6f0e33503a402f79a127e899c0db14596ecf26d
aravind452 <aravindaru452@gmail.com>
2024-10-03 20:26:44 +0530

    added array rotataion

---
+++ QSPIDERS/JAVA/ArrayRotation.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class ArrayRotation {
+
+  public static void main(String[] args) {
+    int[] arr = { 10, 20, 30, 40, 50 };
+    int n = 3;
+    int[] res = arrayRotation(arr, n);
+    System.out.println(Arrays.toString(res));
+  }
+
+  static int[] arrayRotation(int[] arr, int n) {
+
+    int[] res = new int[arr.length];
+
+    int i = 0;
+    int j = 0;
+
+    for (j = n; j < arr.length; j++) {
+      res[i] = arr[j];
+      i++;
+
+    }
+
+    for (int k = 0; k < n; k++) {
+      res[i] = arr[k];
+      i++;
+
+    }
+
+    return res;
+
+  }
+
+}
